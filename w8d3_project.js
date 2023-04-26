18d4a10f1361670ad05c22e93ae18a9d861bd318
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-26 16:27:29 -0400

    myBind

--- w8d3_project.js
+++ w8d3_project.js
@@ -1,68 +1,96 @@
-function Clock() {
-    let time = new Date();
-    this.hours = time.getHours();
-    this.minutes = time.getMinutes();
-    this.seconds = time.getSeconds();
-    this.printTime()
-    setInterval(this._tick.bind(this), 1000);
+// function Clock() {
+//     let time = new Date();
+//     this.hours = time.getHours();
+//     this.minutes = time.getMinutes();
+//     this.seconds = time.getSeconds();
+//     this.printTime()
+//     setInterval(this._tick.bind(this), 1000);
 
-}
-
-// setInterval = function (callback, time) {
-//     looping this amount of time
-//     callback() //this._tick()
 // }
-// window.function() //func style which means we lose context
-// Object.function() //method style which means we keep context 
-// new Function() //constructor style 
 
-Clock.prototype.printTime = function () {
-    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+// // setInterval = function (callback, time) {
+// //     looping this amount of time
+// //     callback() //this._tick()
+// // }
+// // window.function() //func style which means we lose context
+// // Object.function() //method style which means we keep context 
+// // new Function() //constructor style 
+
+// Clock.prototype.printTime = function () {
+//     console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+// }
+
+// Clock.prototype._tick = function () {
+//     if (this.seconds === 60) {
+//         this.seconds = 1;
+//         this.minutes += 1;
+//     } else {
+//         this.seconds += 1; 
+//     }
+
+//     if (this.minutes === 59) {
+//         this.minutes = 0;
+//         this.hours += 1;
+//     } 
+
+//     if (this.hours === 24) {
+//         this.hours = 0;
+//     } 
+//     debugger
+//     this.printTime();
+// }
+
+// const { rawListeners } = require("process");
+// // const clock = new Clock();
+
+// const readline = require("readline");
+// const reader = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// function addNumbers(sum, numsLeft, completionCallback) {
+//     if (numsLeft > 0) {
+//         reader.question('Input a number  ' , answer => {
+//             addNumbers(sum += parseInt(answer), --numsLeft, completionCallback);
+//         })
+//     }
+//     if (numsLeft === 0) {
+//         completionCallback(sum)
+//         reader.close()
+//     }
+// }
+
+// function completionCallback(sum) {
+//     return sum
+// }
+
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+Function.prototype.myBind = function(context) {
+    let fn = this;
+    return fn.apply(context);
+        
 }
 
-Clock.prototype._tick = function () {
-    if (this.seconds === 60) {
-        this.seconds = 1;
-        this.minutes += 1;
-    } else {
-        this.seconds += 1; 
+
+
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
     }
-
-    if (this.minutes === 59) {
-        this.minutes = 0;
-        this.hours += 1;
-    } 
-
-    if (this.hours === 24) {
-        this.hours = 0;
-    } 
-    debugger
-    this.printTime();
-}
-
-const { rawListeners } = require("process");
-// const clock = new Clock();
-
-const readline = require("readline");
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function addNumbers(sum, numsLeft, completionCallback) {
-    if (numsLeft > 0) {
-        reader.question('Input a number  ' , answer => {
-            addNumbers(sum += parseInt(answer), --numsLeft, completionCallback);
-        })
-    }
-    if (numsLeft === 0) {
-        completionCallback(sum)
-        reader.close()
-    }
-}
-
-function completionCallback(sum) {
-    return sum
-}
-
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+  }
+  
+  const turnOn = function() {
+    console.log("Turning on " + this.name);
+  }
+  
+  const lamp = new Lamp();
+  
+  turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+  boundTurnOn(); // should say "Turning on a lamp"
+  myBoundTurnOn(); // should say "Turning on a lamp"
