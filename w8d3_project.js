ce5e2a0ba9f6f90ba25f3b78dc2ff0b57e41311e
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-26 15:19:35 -0400

    addNumbers

--- w8d3_project.js
+++ w8d3_project.js
@@ -40,4 +40,29 @@ Clock.prototype._tick = function () {
     this.printTime();
 }
 
-const clock = new Clock();
+const { rawListeners } = require("process");
+// const clock = new Clock();
+
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question('Input a number  ' , answer => {
+            addNumbers(sum += parseInt(answer), --numsLeft, completionCallback);
+        })
+    }
+    if (numsLeft === 0) {
+        completionCallback(sum)
+        reader.close()
+    }
+}
+
+function completionCallback(sum) {
+    return sum
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
